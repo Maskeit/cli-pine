4cc4a70b85d189aaf7ffc00233e5107d5d43d149
Miguel <miguelagustin182@gmail.com>
2025-06-19 13:02:47 -0600

    Changes

--- utils/vantiva.py
+++ utils/vantiva.py
@@ -1,4 +1,8 @@
-# utils/vantiva.py
+"""
+vantiva-cli.py
+Command line interface
+Created by Miguel Alejandre / @Maskeit
+"""
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
